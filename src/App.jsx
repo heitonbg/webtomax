b772880f2e5c65560e675563620a944365ad832c
Андрей <iwrawww@gmail.com>
2025-11-09 12:51:30 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App.jsx
 import React, { useEffect, useState } from "react";
 
-const API = "http://localhost:8000";
+const API = "https://servicebotformax-iwrawww.amvera.io";
 
 // Компонент входа по ID
 function LoginForm({ onLogin }) {
@@ -1108,4 +1108,5 @@ export default function App() {
   );
 
 }
+
 
