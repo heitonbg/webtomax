23dd4432e36b34cce99ff3a366d5ce9d6ccd655c
Андрей <iwrawww@gmail.com>
2026-04-20 21:04:47 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const API = "https://botik-iwrawww.amvera.io/";
+const API = "https://botik-iwrawww.amvera.io";
 
 function LoadingScreen() {
   return (
